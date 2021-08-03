b1dc0c8af34fa1a3ed9cacd48de02429b2e55503
josokinas <josokinas@users.noreply.github.com>
2021-08-03 13:07:14 +0000

    deploy: 47f5b29ee6d6ae5d443057fb2ce6fae7ec9bc029

---
+++ index.fb4b85a5.js
@@ -0,0 +1,2 @@
+var e,{render:r}=require("react-dom"),t=require("react"),n=(e=t)&&e.__esModule?e.default:e,{StrictMode:l}=t;(async()=>{r(n.createElement(l,null,n.createElement("p",null,"Micro Frontend A")),document.getElementById("root"))})();
+//# sourceMappingURL=index.fb4b85a5.js.map
